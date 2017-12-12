fdda0e7bd616cea60ff07b2cb75e72bff56aeb07
FernandaFT <nanda199784@gmail.com>
2017-12-12 16:46:26 -0600

    Vega Navascues

---
+++ grav/cache/compiled/files/825dd23dd0372cf399b21ddf4104d3d5.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Applications/MAMP/htdocs/grav/user/config/security.yaml',
+    'modified' => 1513118622,
+    'data' => [
+        'salt' => 'aQ5X8pxHFCEyB7'
+    ]
+];
