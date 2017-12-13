4cf519d737666e4a34c32bff43cb36736b06409e
emilio <lcalderon@b7-05.local>
2017-12-13 10:00:34 -0600

    Delete cache

--- grav/cache/compiled/files/92407ab2eaba58300027d516f5fef3da.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Applications/MAMP/htdocs/VN/grav/user/config/media.yaml',
-    'modified' => 1509388779,
-    'data' => [
-        
-    ]
-];
--- grav/cache/compiled/files/d785082db2bc3e363404b801003fbbcc.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Applications/MAMP/htdocs/VN/grav/user/plugins/devtools/devtools.yaml',
-    'modified' => 1509398779,
-    'data' => [
-        'enabled' => true
-    ]
-];
